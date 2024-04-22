39762b2422ee02325bb8b295a7c72b47c6ea9698
小书包 <550083126@qq.com>
2024-04-22 20:43:19 +0800

    T

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -109,7 +109,7 @@ export default function Login({
           className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
           pendingText="Signing Up..."
         >
-          Sign Up1
+          Sign Up
         </SubmitButton>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
