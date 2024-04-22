b6c5b7d85ffa7896f6c955d62bda6658e1b1f9e9
小书包 <550083126@qq.com>
2024-04-23 06:39:48 +0800

    M

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,18 @@ import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
 
+const canInitSupabaseClient = () => {
+  // This function is just for the interactive tutorial.
+  // Feel free to remove it once you have Supabase connected.
+  try {
+    createClient();
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
 
   const isSupabaseConnected = canInitSupabaseClient();
 
@@ -31,7 +32,6 @@ export default async function Index() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
         </main>
       </div>
