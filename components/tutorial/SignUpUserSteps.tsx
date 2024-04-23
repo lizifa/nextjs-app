556c14780f9ff082d196015d5cd519d21d25fc23
小书包 <550083126@qq.com>
2024-04-23 14:24:57 +0800

    M

--- components/tutorial/SignUpUserSteps.tsx
+++ components/tutorial/SignUpUserSteps.tsx
@@ -1,10 +1,8 @@
-import Link from "next/link";
-import Step from "./Step";
-
 export default function SignUpUserSteps() {
   return (
     <div className="flex flex-div gap-6">
-      游客1
+      Hello stranger
+
     </div>
   );
 }
