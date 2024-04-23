db22ecab68b5e965ce4528925ff8b3959273d1bc
小书包 <550083126@qq.com>
2024-04-23 15:26:41 +0800

    M

--- components/tutorial/SignUpUserSteps.tsx
+++ components/tutorial/SignUpUserSteps.tsx
@@ -1,6 +1,6 @@
 export default function SignUpUserSteps() {
   return (
-    <div className="flex flex-div gap-6">
+    <div className="flex flex-div gap-6" style={{ 'fontSize': '30px', 'fontWeight': 600 }}>
       Hello stranger
 
     </div>
